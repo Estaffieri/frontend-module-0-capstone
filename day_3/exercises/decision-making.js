a2cd3de522e8a2b93d46d6ccb2c9498e6ccd3fc5
Estelle Staffieri <estellelareinne@gmail.com>
2020-05-02 15:24:51 -0600

    Add Day 3 Work

--- day_3/exercises/decision-making.js
+++ day_3/exercises/decision-making.js
@@ -10,7 +10,7 @@ Spend some time changing the variables and running the file to see how the story
 
 var doorChoice = 1;
 var bearClothing = "";
-var bearChoice = 1;
+var bearChoice = 7;
 
 console.log("You enter a dark room with two doors. Do you go through #1 or #2?");
 
@@ -31,7 +31,7 @@ if (bearChoice === 1) {
   console.log("You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!");
 } else if (bearChoice === 2) {
   console.log("You tell the bear the " + bearClothing + " is too small and it starts to cry!");
-} else if (bearClothing === 3) {
+} else if (bearChoice === 3) {
   console.log("You run as fast as you can into the next room. It's full of snakes!");
 } else {
   console.log("You stay with the bear and become it's best friend!");
@@ -40,16 +40,29 @@ if (bearChoice === 1) {
 /*
 Reflection Questions
 1. In English, using technical vocabulary, describe what is happening between lines 17 and 21.
+    This if...else statement is evaluating if the condition of doorChoice is stricly equal to 1, returning as true,
+    the variable of bearClothing will return as the string "hat". If this condition is false the variable
+    bearClothing will return as the string "scarf".
 
 2. What variable has a new value assigned to it after the first if statement executes?
+    bearClothing
 
 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+    bearClothing would be "scarf"
 
 4. In English, using technical vocabulary, describe what is happening between lines lines 31 and 38.
+    This if...else statement first checks if the condition of the bearChoice variable is true in being stricly equal to 1.
+    If found to be true the console logs a concatenated response of strings and the variable bearClothing. If false, it proceeds
+    to the else if statement with the condition that bearChoice is stricly equal to 2. If this is true an alternate concatenated
+    response is logged to the console. This procees is repeated as the script runs and checks that conditions are true or false.
+    If the variable bearChoice is not equal to 1,2, or 3, so returns as false, the else statemtent is logged to the console.
 
 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+    This would return "You run as fast as you can into the next room. It's full of snakes!"
 
 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
+    The bear is trying on a hat that is too small. You let the bear know and the bear starts to cry.
 
 7. What is your favorite ending?
-*/
+*/ I thought the idea of a bear trying on a scarf that was too small was a funny image, but I didn't want to make the bear cry.
+    I opted for being best friends with the bear tying on a hat.
--- day_3/questions.md
+++ day_3/questions.md
@@ -1,13 +1,45 @@
 ## Day 3 Questions
 
 1. What is a conditional statement? Give three examples.
+    A conditional statement is based on a concept of if/then/else; if a specified condition is met, then your code runs one or more statements, else your code skips or does something different.These evaluate to true or false. Examples include the if statement, else...if, and switch.
+
+    if (credit >= 700) {
+      document.write ("You're Approved!");
+    } else {
+      document.write ( "We are unable to approve your loan at this time");
+    }
+
+    if (orderTotal >= 100) {
+      document.write ("You've received a free gift with purchase");
+    } else {
+      document.write ("");
+    }
+
+    if (currentDate!=== 0601) {
+      document.write ("Happy Birthday!");
+    } else {
+      document.write ("");
+    }
+
 
 1. Why might you want to use an if-statement?
+    The If statement evaluates or looks for a condition. If the condition is found to be true the code block is run. If statements allow for different scenarios to be available, but not always run if inappropriate. It can also allow for greater personalization, accessibility and automation.
 
 1. What is the JavaScript syntax for an if statement?
-
+    if ("condition") {
+      code to run if condition is true;
+    }
 1. How do you add multiple conditions to an if statement?
+    Logical operators && and ||
 
 1. What is the JavaScript syntax for an if/else if/else statement?
+    if ("condition") {
+      if code block
+    }
+    else {
+      else if code block ();
+    }
 
 1. Other than an if-statement, can you think of any other ways we might want to use a conditional statement?
+
+  I'm not sure if you are asking more broadly or this is a point to talk about switch statements. Switch statements allow for a code block with multiple options should the conditions match. So essentially in each scenario this will happen or if no scenario fits run this default.
