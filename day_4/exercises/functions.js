44a6856ea7a50ebe87a988b407856e6ff877562c
Estelle Staffieri <estellelareinne@gmail.com>
2020-05-02 17:42:13 -0600

    Add Day 4 Work

--- day_4/exercises/functions.js
+++ day_4/exercises/functions.js
@@ -5,26 +5,53 @@ file from your terminal with the command `node day_4/exercises/functions.js`
 */
 
 //example: Write a function below that, when called will print your name
-function printName() {
-  console.log("Severus Snape");
-};
-
-printName();
+//function printName() {
+//  console.log("Estelle Staffieri");
+//
+//printName();
 
 // Update the function below so that it takes an argument of your name and
 // prints your name
 
-function printName() { // hint.. add a parameter on this line :)
-  // YOUR CODE HERE
+function printName(firstName, lastName)
+ {var firstName = "Estelle";
+  var lastName = "Staffieri";
+  var fullName = firstName + " " + lastName;
+console.log(fullName)
 };
 
-printName("Albus Dumbledore");
+printName();
 
 // Write a function that takes in 2 numbers as arguments and prints their sum, and
 // then call that function.
-//YOUR CODE HERE
+function score(numOne,numTwo){
+  numOne=10;
+  numTwo=7;
+  sums = numOne + numTwo;
+  console.log(sums)
+}
+score();
 
 // Write a function that takes in two strings and prints a concatenation
 // of those two strings, for example the arguments could be (man, woman) and
 // the end result might be "When Harry Met Sally".  Then, call that function.
-// YOUR CODE HERE
+function Printheadline(greeting, firstName)
+ {var greeting = "I see the assassins have failed.";
+  var firstName = "Charlotte";
+  var humor = "Good morning " + firstName + " " + greeting;
+console.log(humor)
+}
+
+Printheadline();
+
+function hello(){
+  console.log("Sam I am");
+}
+hello();
+
+function hello_someone(){
+  name = "Sam";
+  console.log(name + " I am!");
+}
+
+hello_someone();
--- day_4/questions.md
+++ day_4/questions.md
@@ -1,15 +1,29 @@
 ## Day 4 Questions
 
 1. In your own words, what is the purpose of a function?
+    A function groups together statements related to a specific task. Through calling it can be reused throughout a program without having to write the function again. I keep thinking of it like a stamp.
 
-1. What is a parameter? 
+1. What is a parameter?
+    Information passed into and needed by the function to answer accurately.
 
 1. What is a return value?
+    The response from a function.
 
 1. In the space below, create a function named `hello` that will print `"Sam I am"`.
-
-1. Create a function name `hello_someone` that takes an argument of `name` and logs `name + " I am"`.
+    function hello(){
+      console.log("Sam I am");
+    }
+    hello();
+1. Create a function named `hello_someone` that takes an argument of `name` and logs `name + " I am"`.
+    function hello_someone(){
+      name = "Sam";
+      console.log(name + " I am!");
+    }
 
 1. How would you call or execute the function that you created above?
+    hello_someone();
 
 1. What questions do you still have about functions in Javascript?
+  I feel like some of the examples I created are duplicating from arguments declared in the function and then also in the console.log portion. I want to play around with them and see where the answer is printing from, but I don't feel I have the time at the moment.
+
+  How do we recognize when it is appropriate to use a function when building a program? 
